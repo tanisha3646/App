6e2fd6a9137bea90f49cefa7ec75c5bea15c9849
Tanisha Jain <tanishajain3646@gmail.com>
2023-12-19 22:27:20 +0530

    changes
    changed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Alert from './Components/Alert';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-// import About from './Components/About';
-// import {
-//   BrowserRouter as Router,
-//   Routes as Switch,
-//   Route
-// } from "react-router-dom";
+import About from './Components/About';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route
+} from "react-router-dom";
 import { useState } from 'react';
 
 function App() {
@@ -30,30 +30,30 @@ function App() {
         setMode('dark');
         document.body.style.backgroundColor = '#042743';
         showAlert('Dark mode has been enabled', 'success');
-        document.title = 'Text Utility - Dark';
+        // document.title = 'Text Utility - Dark';
       }
       else{
         setMode('light');
         document.body.style.backgroundColor = 'white';
         showAlert('Light mode has been enabled', 'success')
-        document.title = 'Text Utility - Dark';
+        // document.title = 'Text Utility - Dark';
       }
   }
   return (
     <>
-    {/* <Router> */}
+    <Router>
       {/* <Navbar title = 'TextUtils' about = 'About TextUtils'/> */}
       <Navbar title = 'TextUtils' mode = {mode} toggleMode = {toggleMode}/>
       <Alert alert={alert}/>
       <div className="container my-3">
-      {/* <Switch> */}
-        {/* <Route exact path="/" element={ */}
-        <TextForm heading = 'Enter Text to analyze' mode = {mode} showAlert = {showAlert}/>
-        {/* } /> */}
-        {/* <Route exact path="/about" element={<About/>} /> */}
-      {/* </Switch> */}
+      <Switch>
+        <Route exact path="/" element={ 
+        <TextForm heading = 'Try Text Utility - Word Counter, Character Counter, Remove Extra Spaces, Change to Lowercase, Change to upperCase' mode = {mode} showAlert = {showAlert}/>
+         } /> 
+        <Route exact path="/about" element={<About  mode = {mode}/>} /> 
+      </Switch>
       </div>
-      {/* </Router> */}
+      </Router>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,10 +2,10 @@ import React,{useState} from 'react'
 
 export default function TextForm(props) {
     const [text,setText] = useState('');
-    const handleUpOnchng = (event)=>{
+    const handleOnchng = (event)=>{
         setText(event.target.value);
     }
-    const handleUpOnclk = ()=>{
+    const handleTogOnclk = ()=>{
         if(text.match(/[a-z]/)){
             let newText = text.toUpperCase();
             setText(newText);
@@ -18,21 +18,51 @@ export default function TextForm(props) {
         }
         
     }
+    const handleLowOnclk = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert('Converted to LowerCase', 'success');
+    }
+    const handleUpOnclk = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert('Converted to UpperCase', 'success');
+        
+    }
+    const handleClrOnclk = ()=>{
+        setText("");
+        props.showAlert('Text Cleared', 'success');
+        
+    }
+    const handleCpyOnclk = ()=>{
+        navigator.clipboard.writeText(text);
+        props.showAlert('Text copied', 'success');
+    }
+    const handleRemOnclk = ()=>{
+        let newText = text.split(" ").filter((element) =>{return  element.length!==0}).join(" ");
+        setText(newText);
+        props.showAlert('Extra spaces removed', 'success');
+    }
   return (
     <>
         <div className="mb-3" style = {{color: props.mode==='light'?'black':'white'}}>
-            <h1>{props.heading}</h1>
-            <textarea className="form-control" onChange={handleUpOnchng} style = {{backgroundColor: props.mode==='dark'?'grey':'white',
+            <h1 className = 'mb-4'>{props.heading}</h1>
+            <textarea className="form-control" onChange={handleOnchng} style = {{backgroundColor: props.mode==='dark'?'#13466e':'white',
             color: props.mode==='light'?'black':'white'}} id="textArea" value={text}rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpOnclk}>Toggle Case</button>
+        <button disabled = {text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpOnclk}>Convert to UpperCase</button>
+        <button disabled = {text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLowOnclk}>Convert to LowerCase</button>
+        <button disabled = {text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClrOnclk}>Clear Text</button>
+        <button disabled = {text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCpyOnclk}>Copy Text</button>
+        <button disabled = {text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleRemOnclk}>Remove extra spaces</button>
+        <button disabled = {text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleTogOnclk}>Toggle Case</button>
 
-        <div className="container my-3" style = {{color: props.mode==='light'?'black':'white'}}>
+        <div className="container my-3 my-1" style = {{color: props.mode==='light'?'black':'white'}}>
             <h2>Text Summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.08*text.split(" ").length} Minutes Read</p>
+            <p>{text.split(" ").filter((element) =>{return  element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.08*text.split(" ").filter((element) =>{return  element.length!==0}).length} Minutes Read</p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:'Enter text in the textBox to preview here'}</p>
+            <p>{text.length>0?text:'Nothing to Preview!!!'}</p>
         </div>
     </>
   )
